c5ae3bcf738df584227caf7a9b8c8feebb8d74a9
SukJin Kim <rkathrwks@gmail.com>
2024-11-09 21:11:31 +0900

    feat(util): 상품 정보가 담긴 파일을 읽는 유틸리티 클래스 구현

---
+++ src/main/java/store/util/ProductReader.java
@@ -0,0 +1,25 @@
+package store.util;
+
+import static store.exception.ExceptionMessage.PRODUCT_FILE_READING_FAIL;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ProductReader {
+
+    private ProductReader() {
+    }
+
+    public static List<String> read(Path path) {
+        List<String> productInfo;
+        try (Stream<String> lines = Files.lines(path)) {
+            productInfo = lines.skip(1).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(PRODUCT_FILE_READING_FAIL.getMessage());
+        }
+        return productInfo;
+    }
+}
