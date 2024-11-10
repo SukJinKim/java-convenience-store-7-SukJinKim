ef7878f5714d83224f07cdbfa3159e746af19217
SukJin Kim <rkathrwks@gmail.com>
2024-11-10 21:30:54 +0900

    feat(service): 주문 검증(상품명 존재여부, 재고 수량 초과여부) 기능 구현

--- src/main/java/store/exception/ExceptionMessage.java
+++ src/main/java/store/exception/ExceptionMessage.java
@@ -10,7 +10,9 @@ public enum ExceptionMessage {
     INVALID_PRODUCT_FORMAT("상품 정보의 형식이 올바르지 않습니다. 프로그램을 종료합니다."),
     NO_SUCH_PROMOTION("상품 정보에 기재된 프로모션이 존재하지 않습니다. 프로그램을 종료합니다."),
     DUPLICATE_PRODUCT_ERROR("상품 정보에 중복된 상품이 있거나 동일 상품에 여러 프로모션이 적용되었습니다. 프로그램을 종료합니다."),
-    INVALID_ORDER_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+    INVALID_ORDER_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    NO_SUCH_PRODUCT("존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    OUT_OF_STOCK("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
     
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/store/service/ProductService.java
+++ src/main/java/store/service/ProductService.java
@@ -1,12 +1,18 @@
 package store.service;
 
 import static store.exception.ExceptionMessage.DUPLICATE_PRODUCT_ERROR;
+import static store.exception.ExceptionMessage.NO_SUCH_PRODUCT;
+import static store.exception.ExceptionMessage.OUT_OF_STOCK;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 import store.dto.Inventory;
 import store.dto.InventoryItem;
+import store.model.Order;
+import store.model.Orders;
 import store.model.Product;
 import store.model.Products;
 import store.model.Promotions;
@@ -25,11 +31,10 @@ public class ProductService {
         return products.get();
     }
 
-    public Products registerProductOf(Path path, Promotions promotions) {
+    public void registerProductOf(Path path, Promotions promotions) {
         List<String> fileContents = PromotionReader.read(path);
         List<List<String>> parsed = fileContents.stream().map(ProductParser::parse).toList();
         registerProductOf(parsed, promotions);
-        return products;
     }
 
     public Inventory createInventory() {
@@ -38,6 +43,11 @@ public class ProductService {
         List<InventoryItem> inventoryItems = createInventoryItems();
         inventoryItems.forEach(inventory::add);
         return inventory;
+    }
+
+    public void reviewOrders(Orders orders) {
+        validateProductExistence(orders);
+        validateStockAvailability(orders);
     }
 
     private void registerProductOf(List<List<String>> parsed, Promotions promotions) {
@@ -58,5 +68,42 @@ public class ProductService {
 
     private void update() {
         products.removeIf(Product::isNotForSale);
+    }
+
+    private void validateProductExistence(Orders orders) {
+        Set<String> actualProductNames = getProductNames();
+
+        orders.get().stream()
+                .map(Order::getProductName)
+                .filter(productName -> !actualProductNames.contains(productName))
+                .findFirst()
+                .ifPresent(invalidName -> {
+                    throw new NoSuchElementException(NO_SUCH_PRODUCT.getMessage());
+                });
+    }
+
+    private Set<String> getProductNames() {
+        return products.get().stream()
+                .map(Product::getName)
+                .collect(Collectors.toSet());
+    }
+
+    private void validateStockAvailability(Orders orders) {
+        for (Order order : orders.get()) {
+            String orderedProductName = order.getProductName();
+            int orderedQuantity = order.getQuantity();
+            int stockQuantity = getStockQuantity(orderedProductName);
+
+            if (orderedQuantity > stockQuantity) {
+                throw new IllegalArgumentException(OUT_OF_STOCK.getMessage());
+            }
+        }
+    }
+
+    private int getStockQuantity(String orderedProductName) {
+        return products.get().stream()
+                .filter(product -> product.getName().equals(orderedProductName))
+                .mapToInt(Product::getQuantity)
+                .sum();
     }
 }
