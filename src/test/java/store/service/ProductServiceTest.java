ae64e7b37d16c1d3c8ac3c4847d28a2fcf63b6a4
SukJin Kim <rkathrwks@gmail.com>
2024-11-10 21:41:09 +0900

    test(service): 주문 검증(상품명 존재여부, 재고 수량 초과여부) 기능 유닛 테스트 추가

--- src/test/java/store/service/ProductServiceTest.java
+++ src/test/java/store/service/ProductServiceTest.java
@@ -1,21 +1,28 @@
 package store.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static store.constant.FilePath.PRODUCT_FILE_PATH;
 import static store.exception.ExceptionMessage.DUPLICATE_PRODUCT_ERROR;
+import static store.exception.ExceptionMessage.NO_SUCH_PRODUCT;
+import static store.exception.ExceptionMessage.OUT_OF_STOCK;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import store.dto.Inventory;
 import store.dto.InventoryItem;
+import store.model.Orders;
 import store.model.Product;
 import store.model.Promotion;
 import store.model.Promotions;
@@ -23,6 +30,7 @@ import store.model.Promotions;
 class ProductServiceTest {
     private ProductService productService;
     private Promotions promotions;
+    private OrderService orderService;
 
     @BeforeEach
     void setUp() {
@@ -34,6 +42,7 @@ class ProductServiceTest {
                 LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2)));
         promotions.add(new Promotion("반짝할인", 1, 1,
                 LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1)));
+        orderService = new OrderService();
     }
 
     @Test
@@ -118,5 +127,65 @@ class ProductServiceTest {
         assertEquals(1000, item.getPrice());
         assertEquals(10, item.getQuantity());
         assertEquals("MD추천상품", item.getPromotionName());
+    }
+
+    @ParameterizedTest(name = "{arguments} 입력")
+    @ValueSource(strings = {"[콜라-20],[사이다-5]", "[콜라-5],[사이다-15]"})
+    void reviewOrders_정상_테스트(String userInput) throws IOException {
+        Path tempFile = Files.createTempFile("products", ".md");
+        Files.write(tempFile, List.of(
+                "name,price,quantity,promotion",
+                "콜라,1000,10,탄산2+1",
+                "콜라,1000,10,null",
+                "사이다,1000,8,탄산2+1",
+                "사이다,1000,7,null"
+        ));
+
+        productService.registerProductOf(tempFile, promotions);
+        Orders orders = orderService.createOrders(userInput);
+
+        assertDoesNotThrow(() -> {
+            productService.reviewOrders(orders);
+        });
+    }
+
+    @ParameterizedTest(name = "{arguments} 입력")
+    @ValueSource(strings = {"[감자칩-20]", "[제로콜라-5]"})
+    void reviewOrders_존재하지_않는_상품_예외_테스트(String userInput) throws IOException {
+        Path tempFile = Files.createTempFile("products", ".md");
+        Files.write(tempFile, List.of(
+                "name,price,quantity,promotion",
+                "콜라,1000,10,탄산2+1",
+                "콜라,1000,10,null",
+                "사이다,1000,8,탄산2+1",
+                "사이다,1000,7,null"
+        ));
+
+        productService.registerProductOf(tempFile, promotions);
+        Orders orders = orderService.createOrders(userInput);
+
+        assertThatThrownBy(() -> {
+            productService.reviewOrders(orders);
+        }).isInstanceOf(NoSuchElementException.class).hasMessage(NO_SUCH_PRODUCT.getMessage());
+    }
+
+    @ParameterizedTest(name = "{arguments} 입력")
+    @ValueSource(strings = {"[콜라-21]", "[사이다-16]"})
+    void reviewOrders_재고수량_초과_예외_테스트(String userInput) throws IOException {
+        Path tempFile = Files.createTempFile("products", ".md");
+        Files.write(tempFile, List.of(
+                "name,price,quantity,promotion",
+                "콜라,1000,10,탄산2+1",
+                "콜라,1000,10,null",
+                "사이다,1000,8,탄산2+1",
+                "사이다,1000,7,null"
+        ));
+
+        productService.registerProductOf(tempFile, promotions);
+        Orders orders = orderService.createOrders(userInput);
+
+        assertThatThrownBy(() -> {
+            productService.reviewOrders(orders);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(OUT_OF_STOCK.getMessage());
     }
 }
