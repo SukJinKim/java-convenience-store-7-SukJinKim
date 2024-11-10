425d65ee8decd471952e811068b8b95b427a722d
SukJin Kim <rkathrwks@gmail.com>
2024-11-10 09:15:19 +0900

    feat(controller): 상품 등록 기능 구현

---
+++ src/main/java/store/controller/StoreController.java
@@ -0,0 +1,34 @@
+package store.controller;
+
+import static store.constant.FilePath.PRODUCT_FILE_PATH;
+import static store.constant.FilePath.PROMOTION_FILE_PATH;
+
+import store.model.Products;
+import store.model.Promotions;
+import store.service.ProductService;
+import store.service.PromotionService;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class StoreController {
+    private InputView inputView;
+    private OutputView outputView;
+    private PromotionService promotionService;
+    private ProductService productService;
+
+    public StoreController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.promotionService = new PromotionService();
+        this.productService = new ProductService();
+    }
+
+    public void run() {
+        Products products = registerProduct();
+    }
+
+    private Products registerProduct() {
+        Promotions promotions = promotionService.registerPromotionFrom(PROMOTION_FILE_PATH.getPath());
+        return productService.registerProductFrom(PRODUCT_FILE_PATH.getPath(), promotions);
+    }
+}
