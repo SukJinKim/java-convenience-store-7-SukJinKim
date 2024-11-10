db055d20c29e4f6f71bac4cde26625681d8279e8
SukJin Kim <rkathrwks@gmail.com>
2024-11-10 10:55:12 +0900

    feat(view): 환영인사 및 현재 보유하고 있는 상품 출력기능 구현

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,17 @@
+package store.view;
+
+import store.dto.Inventory;
+
+public class OutputView {
+    private static final String HELLO = "안녕하세요. W편의점입니다.";
+    private static final String GUIDE_INVENTORY = "현재 보유하고 있는 상품입니다.";
+
+    public void sayHello() {
+        System.out.println(HELLO);
+    }
+
+    public void showInventory(Inventory inventory) {
+        System.out.println(GUIDE_INVENTORY + System.lineSeparator());
+        System.out.println(inventory);
+    }
+}
